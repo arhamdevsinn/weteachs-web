63b87a58fece98cda883893c213e1aaa4253d4e2
arhamdevsinn <111589538+arhamdevsinn@users.noreply.github.com>
2026-01-19 20:13:21 +0500

    latest

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -6,6 +6,7 @@ export default function ForgotPasswordPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-secondary">
       <ForgotPassword />
+      
     </div>
   );
 }
